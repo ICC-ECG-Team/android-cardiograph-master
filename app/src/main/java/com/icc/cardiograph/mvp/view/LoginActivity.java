9ebb6f2ee4dc5e56d2eaa09714a91ed9c557646e
yejinxin <yejx@newlandpayment.com>
2017-03-22 11:21:11 +0800

    (*)优化代码

--- app/src/main/java/com/icc/cardiograph/mvp/view/LoginActivity.java
+++ app/src/main/java/com/icc/cardiograph/mvp/view/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.InputFilter.LengthFilter;
 import android.text.InputType;
